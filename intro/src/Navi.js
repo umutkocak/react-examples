e43038309c30847292d73d08944063a77d5290b0
Umut Koçak <ukocak34@hotmail.com>
2020-03-29 18:17:39 +0300

    fix bug navi

--- intro/src/Navi.js
+++ intro/src/Navi.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import {
   Collapse,
   Navbar,
@@ -6,58 +6,52 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
 } from "reactstrap";
-import CartSummary from "./CartSummary";
 import { Link } from "react-router-dom";
+import CartSummary from "./CartSummary";
 
-export default class Navi extends Component {
-  constructor(props) {
-    super(props);
+const Example = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand>
-            <Link to="">intro</Link>
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.isOpen} navbar>
-            <Nav navbar>
-              <NavItem>
-                <NavLink>
-                  <Link to="cart">Cart </Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>
-                  <Link to="form1">Form 1 </Link>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink>
-                  <Link to="form1-2">Form 2 </Link>
-                </NavLink>
-              </NavItem>
-              <CartSummary
-                cart={this.props.cart}
-                removeFromCart={this.props.removeFromCart}
-              />
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
-}
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink>
+                <Link to="cart">Cart </Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="form1">Form 1 </Link>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink>
+                <Link to="form1-2">Form 2 </Link>
+              </NavLink>
+            </NavItem>
+            <CartSummary
+              cart={props.cart}
+              removeFromCart={props.removeFromCart}
+            />
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Example;
